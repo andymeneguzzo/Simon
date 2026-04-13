7a557a4a484668d4677fd1fd80981b6937a4cacc
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-04-13 10:32:45 +0200

    impl: setting up the MainActivity.kt following instructions on interaction and instance state preservation

--- app/src/main/java/com/andres/simon_project/MainActivity.kt
+++ app/src/main/java/com/andres/simon_project/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.andres.simon_project
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    /* Global variables for UI elements */
+
+    /* non-editable TextView printing the clicked sequence */
+    private lateinit var textViewSequence: TextView
+
+    /* Colored Buttons (actually clickable TextViews) */
+    private lateinit var buttonRed: TextView
+    private lateinit var buttonGreen: TextView
+    private lateinit var buttonBlue: TextView
+    private lateinit var buttonMagenta: TextView
+    private lateinit var buttonYellow: TextView
+    private lateinit var buttonCyan: TextView
+
+    /* Buttons */
+    private lateinit var buttonCancel: Button
+    private lateinit var buttonEndOfGame: Button
 
     /* onCreate method being called at app launch */
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +39,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        /* Bind variables with their UI elements (found by ID) */
+        bindUIViews()
+
+        /* Ensure instance state is preserved when orientation changes */
+        handleInstanceState(savedInstanceState)
+
+        /* Set interaction listeners for clickable colored TextViews and Buttons  */
+        setInteractionListeners()
+
+        /* Keep sequence updated after every click of the colored TextViews */
+        printCurrentSequence()
     }
+
+    private fun bindUIViews() {
+        TODO("bind variables with their UI elements")
+    }
+
+    private fun handleInstanceState(savedInstanceState: Bundle?) {
+        TODO("handle Instance State preservation")
+    }
+
+    private fun setInteractionListeners() {
+        TODO("Set all listeners for clickable TextViews and Buttons")
+    }
+
+    private fun printCurrentSequence() {
+        TODO("Enable sequence update after every click of the colored TextViews")
+    }
+
 }
