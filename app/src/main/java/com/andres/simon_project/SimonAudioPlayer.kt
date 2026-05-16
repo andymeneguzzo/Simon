d098b93069167f59fc64f0c123ad7e3bfb64dab6
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-05-16 16:18:06 +0200

    impl: understanding how to implement the audio player with AudioTrack

--- app/src/main/java/com/andres/simon_project/SimonAudioPlayer.kt
+++ app/src/main/java/com/andres/simon_project/SimonAudioPlayer.kt
@@ -3,7 +3,7 @@ package com.andres.simon_project
 /* This class uses AudioTrack to play a sound for each color */
 class SimonAudioPlayer {
 
-    /* set some constant params */
+    /* set some constant params - take into account if constants are class-wise or object-wise */
 
     /* assign a frequency for each color */
 
