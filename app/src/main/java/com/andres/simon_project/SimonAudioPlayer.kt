8f445c8d3f8f76639c80c0b6d4dd003777ac03e0
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-05-16 16:23:56 +0200

    impl: created basic constants for audio handling (sample_rate, duration, volume) and method to assign to each color a different audio frequency

--- app/src/main/java/com/andres/simon_project/SimonAudioPlayer.kt
+++ app/src/main/java/com/andres/simon_project/SimonAudioPlayer.kt
@@ -4,8 +4,25 @@ package com.andres.simon_project
 class SimonAudioPlayer {
 
     /* set some constant params - take into account if constants are class-wise or object-wise */
+    /* using a companion object, the constants will be linked to the class SimonAudioPlayer, not to its object */
+    companion object {
+        private const val SAMPLE_RATE = 44100 // a standard sample rate
+        private const val DURATION_MS = 200 // 200ms of duration
+        private const val VOLUME = 0.5 // volume of 50%
+    }
 
     /* assign a frequency for each color */
+    private fun assignFrequencyToColor(color: String) : Double {
+        return when (color) {
+            "R" -> 260.00
+            "G" -> 330.00
+            "B" -> 392.00
+            "M" -> 494.00
+            "Y" -> 587.00
+            "C" -> 659.00
+            else -> 440.00
+        }
+    }
 
     /* function to play audio (accounting for thread safety) */
 }
