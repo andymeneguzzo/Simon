38e3119777053f7b69bb86b4b1f22f985f1de57e
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-05-19 22:43:06 +0200

    impl: method to get the start index where to start writing the sequence in red

--- app/src/main/java/com/andres/simon_project/MatchTextFormatter.kt
+++ app/src/main/java/com/andres/simon_project/MatchTextFormatter.kt
@@ -5,8 +5,22 @@ package com.andres.simon_project
 object MatchTextFormatter {
 
     /* build the sequence with the new format */
-    fun buildErrorSequenceFormattedText(match: GameSession.Match) {}
+    fun buildErrorSequenceFormattedText(match: GameSession.Match) {
+
+    }
 
     /* find the index where the incorrect sequence starts */
-    private fun getCharStartIndex(sequence: List<String>, index: Int) {}
+    private fun getCharStartIndex(sequence: List<String>, targetIndex: Int) : Int {
+        var idx = 0
+
+        for (index in sequence.indices) {
+            if (index == targetIndex) return idx // target index reached, where to start writing the sequence in red
+
+            idx += sequence[index].length
+
+            if (index < sequence.lastIndex) idx += 2
+        }
+
+        return idx
+    }
 }
