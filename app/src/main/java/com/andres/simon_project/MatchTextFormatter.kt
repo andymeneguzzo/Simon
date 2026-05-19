42f51d6565d7ac44a7a1c0b43a08ab601ade4848
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-05-19 22:37:43 +0200

    impl: creating the object MatchTextFormatter.kt to handle sequence formatting

---
+++ app/src/main/java/com/andres/simon_project/MatchTextFormatter.kt
@@ -0,0 +1,12 @@
+package com.andres.simon_project
+
+/* useful to format match sequences colored in red starting from the errorIndex
+* handled in this separate object file to keep the code as clean as possible */
+object MatchTextFormatter {
+
+    /* build the sequence with the new format */
+    fun buildErrorSequenceFormattedText(match: GameSession.Match) {}
+
+    /* find the index where the incorrect sequence starts */
+    private fun getCharStartIndex(sequence: List<String>, index: Int) {}
+}
