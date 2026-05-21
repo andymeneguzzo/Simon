3b465cc8bfdead2c65fec4442b55160087157568
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2026-05-21 11:33:59 +0200

    impl: setting up the MatchDetailActivity.kt

--- app/src/main/java/com/andres/simon_project/MatchDetailActivity.kt
+++ app/src/main/java/com/andres/simon_project/MatchDetailActivity.kt
@@ -1,4 +1,48 @@
 package com.andres.simon_project
 
-class MatchDetailActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/**
+ * Here we show a single match with more details than the RecyclerView
+ * User can exit using the system back
+ */
+class MatchDetailActivity : AppCompatActivity() {
+
+    // need ID of the match
+    companion object {
+        const val MATCH_ID = "match_id"
+    }
+
+    // DB
+    private lateinit var dbHelper: MatchDBHelper
+
+    // UI
+    private lateinit var textViewPressCount: TextView
+    private lateinit var textViewSequence: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_match_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.matchDetail)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        dbHelper = MatchDBHelper(this)
+        // todo -> bind UI views, render the detail
+    }
+    private fun bindUIViews() {
+
+    }
+    private fun printMatchDetail() {
+
+    }
 }
